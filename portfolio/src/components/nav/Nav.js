a46d80f6ab212d074de7ffe84d77c667c57c4e1b
mcventus <fiseyoum@gmail.com>
2023-02-20 10:22:13 -0500

    responsive

--- portfolio/src/components/nav/Nav.js
+++ portfolio/src/components/nav/Nav.js
@@ -1,18 +1,29 @@
+import { useState } from 'react'
 import logo4 from './logo4.png';
 import './nav.css'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { TfiLayoutMenuSeparated } from 'react-icons/tfi'
 
 const Nav = () =>{
+    const [showNavbar, setShowNavbar] = useState(false);
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar)
+    }
     return(
         <div className="container">
            <nav>
             <img src={logo4} className="logo"></img>
-                <ul>
-                    <li><Link id="link" to="/">Home</Link></li>
-                    <li><Link id="link" to="/about">About</Link></li>
-                    <li><Link id="link" to="/contact">Contact</Link></li>
-                    <li><Link id="link" to="/projects">Projects</Link></li>
-                </ul>
+                <button className='dropdown-btn' onClick={ handleShowNavbar }><TfiLayoutMenuSeparated/></button>
+                <div className={`nav-elements ${showNavbar && 'active'}`}>
+                    <ul>
+                        <li><NavLink id="link" to="/">Home</NavLink></li>
+                        <li><NavLink id="link" to="/about">About</NavLink></li>
+                        <li><NavLink id="link" to="/contact">Contact</NavLink></li>
+                        <li><NavLink id="link" to="/projects">Projects</NavLink></li>
+                    </ul>
+                </div>
+               
            </nav>
            <div className="header-text">
                 <p>Full Stack Software Engineer</p>
