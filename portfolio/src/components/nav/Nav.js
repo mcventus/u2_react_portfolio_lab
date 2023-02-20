38711630e749eabd8277e79bd4102782f9b180ab
mcventus <fiseyoum@gmail.com>
2023-02-20 13:17:56 -0500

    a fix to navbar

--- portfolio/src/components/nav/Nav.js
+++ portfolio/src/components/nav/Nav.js
@@ -1,20 +1,24 @@
 import { useState } from 'react'
 import logo4 from './logo4.png';
 import './nav.css'
+import '../main/main.css'
 // import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import { TfiLayoutMenuSeparated } from 'react-icons/tfi'
 
 const Nav = () =>{
+
     const [showNavbar, setShowNavbar] = useState(false);
     const handleShowNavbar = () => {
         setShowNavbar(!showNavbar)
     }
+
     return(
         <div className="container">
            <nav>
-            <img src={logo4} className="logo"></img>
-                <button className='dropdown-btn' onClick={ handleShowNavbar }><TfiLayoutMenuSeparated/></button>
+            <img src={logo4} className="logo"></img> 
+            <div className='dropdown-menu'>
+            <button className='dropdown-btn' onClick={ handleShowNavbar }><TfiLayoutMenuSeparated/></button>
                 <div className={`nav-elements ${showNavbar && 'active'}`}>
                     <ul>
                         <li><NavLink id="link" to="/">Home</NavLink></li>
@@ -23,7 +27,7 @@ const Nav = () =>{
                         <li><NavLink id="link" to="/projects">Projects</NavLink></li>
                     </ul>
                 </div>
-               
+            </div>
            </nav>
            <div className="header-text">
                 <p>Full Stack Software Engineer</p>
